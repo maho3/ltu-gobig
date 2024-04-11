9434e8f27fea3ff6a5768c8fa321bbc3ba945bb3
Matthew Ho <matthew.annam.ho@gmail.com>
2024-04-11 14:49:17 +0200

    adding density field script from Shivam

---
+++ scripts/quijote_density_field/make_density_field.py
@@ -0,0 +1,25 @@
+"""
+This script reads the quijote snapshot and produces DM density fields, using pylians.
+"""
+
+import numpy as np
+import MAS_library as MASL
+
+root_in = '/home/mattho/data/quijote/Snapshots/latin_hypercube'
+root_out = '/home/mattho/data/quijote/density_field/latin_hypercube'
+ptypes = [1]
+grid = 128
+BoxSize = 1000.0  # Mpc/h ; size of box
+snapnum = 3  # for z=0.5
+# snapnum = 4 #for z=0.0
+sim_LH_nums = [0, 1, 10, 100]
+
+for sim_LH_num in sim_LH_nums:
+    snapshot = f'{root_in}/{sim_LH_num}/snapdir_{snapnum:03d}/snap_{snapnum:03d}'
+    print(snapshot)
+    df_cic = MASL.density_field_gadget(snapshot, ptypes, grid, MAS='CIC',
+                                       do_RSD=False, axis=0, verbose=False)
+    df_pylians_cic = df_cic/np.mean(df_cic, dtype=np.float64)-1.0
+
+    out_file = f'{root_out}/{sim_LH_num}/df_m_{grid}_z={0.5}.npy'
+    np.save(out_file, df_pylians_cic)
