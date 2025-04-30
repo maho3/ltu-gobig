200f77e022b0e1e65d4e35e9cc2db385c090808e
Matthew Ho <matthew.annam.ho@gmail.com>
2025-04-29 22:51:42 -0400

    figure out what's going wrong with abacus bias

--- scripts/preprocess_existing_sims/abacus.py
+++ scripts/preprocess_existing_sims/abacus.py
@@ -1,71 +1,102 @@
 import numpy as np
 import os
 import sys
+from tqdm import tqdm
+from os.path import join
 import abacusnbody.data.asdf
 from abacusnbody.data.compaso_halo_catalog import CompaSOHaloCatalog
+from cmass.bias.rho_to_halo import save_snapshot
 
 
-def halos_to_cmass(rootdir, z, m_min, m_max, all_at_once = True):
-
+def halos_to_cmass(rootdir, z, m_min, m_max, all_at_once=True):
     '''
     doc
     '''
-    z_dict = {0.5:"z0.500"}
+    z_dict = {0.5: "z0.500"}
 
     # snapdir should have a subfolder such that we have the structure "...snapdir/halo_info/halo_info*.asdf"
-    snapdir = os.path.join(rootdir, z_dict[z])
+    snapdir = join(rootdir, 'halos', z_dict[z], 'halo_info')
 
     if all_at_once:
-
-        cat = CompaSOHaloCatalog(snapdir, cleaned = False, fields = ["N", "x_com","v_com"])
+        raise NotImplementedError
+        cat = CompaSOHaloCatalog(snapdir, cleaned=True, fields=[
+                                 "N", "x_com", "v_com"])
         MHsun = cat.header["ParticleMassHMsun"]
         print(MHsun)
         N_min = int(m_min/MHsun)
         print(N_min)
         N_max = int(m_max/MHsun)
         print(N_max)
-        cat = cat.halos[(cat.halos["N"]>=N_min) & (cat.halos["N"]<=N_max)]
+        cat = cat.halos[(cat.halos["N"] >= N_min) & (cat.halos["N"] <= N_max)]
 
         masses = cat["N"].data * MHsun
         pos = cat["x_com"].data
         vel = cat["v_com"].data
     else:
-        slabs = os.listdir(snapdir)
-        masses = np.array([])
-        pos    = np.array([])
-        vel    = np.array([])
+        slabs = sorted(os.listdir(snapdir))
+        masses, pos, vel = [], [], []
 
         # slab is eg halo_info_003.asdf
-        for slab in slabs:
-            cat = CompaSOHaloCatalog(snapdir+ "/" + slab, cleaned = False, fields = ["N", "x_com","v_com"])
+        for i, slab in enumerate(slabs):
+            if 'checksums' in slab:
+                continue
+            filename = join(snapdir, slab)
+            print(f"Processing slab {i+1}/{len(slabs)}: {filename}")
+
+            cat = CompaSOHaloCatalog(
+                filename, cleaned=True,
+                fields=["N", "x_com", "v_com"])
             MHsun = cat.header["ParticleMassHMsun"]
-            print(MHsun)
+            # print(MHsun)
             N_min = int(m_min/MHsun)
             N_max = int(m_max/MHsun)
-            cat = cat.halos[(cat.halos["N"]>=N_min) & (cat.halos["N"]<=N_max)]
+            cat = cat.halos[(cat.halos["N"] >= N_min) &
+                            (cat.halos["N"] <= N_max)]
 
-            masses = np.concatenate((masses,cat["N"].data * MHsun))
-            pos = np.concatenate((pos,cat["x_com"].data))
-            vel = np.concatenate((vel,cat["v_com"].data))
+            masses.append(cat["N"].data * MHsun)
+            pos.append(cat["x_com"].data)
+            vel.append(cat["v_com"].data)
 
-    #assert z == cat.header["Redshift"]
-    return np.log10(masses), pos, vel*(1.0 + z)
+        masses = np.concatenate(masses, axis=0)
+        pos = np.concatenate(pos, axis=0)
+        vel = np.concatenate(vel, axis=0)
+
+        pos += 1000   # to avoid negative positions
+        pos %= 20000  # periodicity
+        #     masses = np.concatenate((masses, cat["N"].data * MHsun), axis=0)
+        #     pos = np.concatenate((pos, cat["x_com"].data), axis=0)
+        #     vel = np.concatenate((vel, cat["v_com"].data), axis=0)
+
+    # assert z == cat.header["Redshift"]
+    return np.log10(masses), pos, vel  # *(1.0 + z)
+
 
 if __name__ == "__main__":
+
+    print(sys.argv)
 
     # Number of threads for abacus API
     Nth = int(sys.argv[1])
     abacusnbody.data.asdf.set_nthreads(Nth)
 
-    m_min = 1e11
-    m_max = 1e15
-    
+    m_min = 10**12.5
+    m_max = 1e17
+
     in_dir = sys.argv[2]
     my_z = float(sys.argv[3])
     out_dir = sys.argv[4]
-    allslabs = sys.argv[5]
+    allslabs = int(sys.argv[5])
 
-    massh, posh, velh = halos_to_cmass(in_dir, my_z, m_min, m_max, all_at_once = allslabs)
-    np.save(os.path.join(out_dir, 'halo_pos.npy'), posh)  # halo positions [Mpc/h]
-    np.save(os.path.join(out_dir, 'halo_vel.npy'), velh)  # halo velocities [km/s]
-    np.save(os.path.join(out_dir, 'halo_mass.npy'), massh)  # halo masses [Msun/h]
+    massh, posh, velh = halos_to_cmass(
+        in_dir, my_z, m_min, m_max, all_at_once=allslabs)
+
+    a = 1.0/(1.0 + my_z)
+    if os.path.isfile(join(out_dir, 'halos.h5')):
+        os.remove(join(out_dir, 'halos.h5'))
+    save_snapshot(out_dir, a, posh, velh, massh)
+    # np.save(os.path.join(out_dir, 'halo_pos.npy'),
+    #         posh)  # halo comoving positions [Mpc/h]
+    # np.save(os.path.join(out_dir, 'halo_vel.npy'),
+    #         velh)  # halo physical velocities [km/s]
+    # np.save(os.path.join(out_dir, 'halo_mass.npy'),
+    #         massh)  # halo masses [Msun/h]
